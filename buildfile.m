b4a76a3bba08cf58d831c29b48f6d4dde0be7718
Harold van Heukelum <harold.van.heukelum@boskalis.com>
2023-06-15 11:36:57 +0200

    remove m-files

--- buildfile.m
+++ buildfile.m
@@ -16,14 +16,14 @@ base = fullfile(getenv("GITHUB_WORKSPACE"),"build", "*.m");
 pcode(base,"-inplace")
 
 classes = fullfile(getenv("GITHUB_WORKSPACE"),"build","classes");
-files = dir(classes)
+files = dir(classes);
 dirFlags = [files.isdir];
 subFolders = files(dirFlags);
 subFolderNames = {subFolders(3:end).name} % Start at 3 to skip . and ..
 
 for k = 1 : length(subFolderNames)
 	fprintf('Sub folder #%d = %s\n', k, subFolderNames{k});
-    subf = fullfile(getenv("GITHUB_WORKSPACE"),"build","classes", subFolderNames{k}, "*.m")
+    subf = fullfile(getenv("GITHUB_WORKSPACE"),"build","classes", subFolderNames{k}, "*.m");
     pcode(subf,'-inplace')
 end
 
