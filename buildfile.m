258ad9348df1facc422f3859dd86cd5e3dfe8197
Harold van Heukelum <harold.van.heukelum@boskalis.com>
2023-06-15 11:04:06 +0200

    loop through classes

--- buildfile.m
+++ buildfile.m
@@ -14,6 +14,20 @@ function buildTask(~)
 % Create pcode
 base = fullfile(getenv("GITHUB_WORKSPACE"),"build", "*.m");
 pcode(base,"-inplace")
-classes = fullfile(getenv("GITHUB_WORKSPACE"),"build","classes","**/*.m");
-pcode(classes,"-inplace")
+
+classes = fullfile(getenv("GITHUB_WORKSPACE"),"build","classes");
+files = dir(classes);
+% Get a logical vector that tells which is a directory.
+dirFlags = [files.isdir];
+% Extract only those that are directories.
+subFolders = files(dirFlags); % A structure with extra info.
+% Get only the folder names into a cell array.
+subFolderNames = {subFolders(3:end).name} % Start at 3 to skip . and ..
+
+for idx = 1:numel(subFolderNames)
+    element = subFolderNames(idx)
+    cls = fullfile(getenv("GITHUB_WORKSPACE"),"build","classes",element,"*.m");
+    pcode(classes,"-inplace");
+end
+
 end
