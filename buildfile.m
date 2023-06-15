99d8e66c0f3fac1bcf0bd31e92b94206a0c4a674
Harold van Heukelum <harold.van.heukelum@boskalis.com>
2023-06-15 10:47:54 +0200

    poging 2

--- buildfile.m
+++ buildfile.m
@@ -13,7 +13,7 @@ end
 function buildTask(~)
 % Create pcode
 base = fullfile(getenv("GITHUB_WORKSPACE"),"build", "*.m");
-pcode(base)
-classes = fullfile(getenv("GITHUB_WORKSPACE"),"build","classes", "*.m");
-pcode(classes)
+pcode(base,"-inplace")
+classes = fullfile(getenv("GITHUB_WORKSPACE"),"build","classes","**/*.m");
+pcode(classes,"-inplace")
 end
